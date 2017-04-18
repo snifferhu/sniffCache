5acb28c4582cd395714796218ff461a681e56eb3
snifferhu <huyong@zhai.me>
2017-04-18 21:28:33 +0800

    test(sharded)

---
+++ src/test/java/ShardedTestCase.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+/**
+ * @auth snifferhu
+ * @date 2017/4/18 21:15
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+/** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **/
+@ContextConfiguration(locations = {"classpath:spring-context.xml"})
+public class ShardedTestCase {
+    @Autowired
+    private ShardedJedisPool shardedJedisPool;
+
+    @Test
+    public void test(){
+        ShardedJedis jedis = shardedJedisPool.getResource();
+        for (int i = 0;i < 100;i++)
+            jedis.set(String.valueOf(i),String.valueOf(i));
+    }
+}
